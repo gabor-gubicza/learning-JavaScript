3a195742901ef6e5658a0c7e093eba43cb5e6e28
Gabor Gubicza <125979376+gabor-gubicza@users.noreply.github.com>
2023-11-13 19:16:17 +0100

    update

--- JavaScript/01_The_Complete_JS_Course_2023/05-Guess-My-Number-starter/script.js
+++ JavaScript/01_The_Complete_JS_Course_2023/05-Guess-My-Number-starter/script.js
@@ -7,11 +7,17 @@ document.querySelector('.score').textContent = 10;
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 */
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = document.querySelector('.guess').value;
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
   }
 });
